a96fc641b955da5e40f62cf2769ba33fd6bb16e7
libin-n-g <libinngeorge@gmail.com>
2017-08-09 20:38:16 +0530

    Q2 First parts

---
+++ Q2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+list = [1,2,3,4,5]
+print type(list)
+nparray = np.array(list)
+print type(nparray)
+array2 = np.array([[1, 2, 3],
+		[5, 6, 7],
+		[8, 9, 10]])
+print "shape of the array is ", array2.shape
+print "dimention of the array is ", array2.ndim
+array3 = np.arange(0, 9).reshape((3,3))
+print array3
+print array2
+print np.add(array2, array3)
+print array2, "*", array3, "=", np.multiply(array2, array3)
